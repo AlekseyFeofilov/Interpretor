c13d557f1b5a7e4b46e85c347bb0c99a75beb682
VanyaVVorobev <ivanvorovev14042004@mail.ru>
2022-05-06 15:14:06 +0700

    add binding to WorkspaceFragment.kt merge BlocksFragment.kt and RightPanelFragments to WorkspaceFragment.kt and refactored them

--- app/src/main/java/com/example/interpreter/WorkspaceFragment.kt
+++ app/src/main/java/com/example/interpreter/WorkspaceFragment.kt
@@ -7,78 +7,88 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import com.example.interpreter.databinding.FragmentBlocksBinding
+import com.example.interpreter.databinding.FragmentRightPanelBinding
+import com.example.interpreter.databinding.FragmentWorkspaceBinding
+import com.example.interpreter.vm.instruction.Bool
 
 var isConsoleHidden = true
+var isBlocksPanelHidden = true
 
 data class Point(var x:Float, var y:Float)
 
 class WorkspaceFragment : Fragment(R.layout.fragment_workspace) {
     private val touchPoint = Point(0f, 0f)
     private var isPanelMoving = false
-    private lateinit var console: FrameLayout
-    private val fragmentPalette = BlocksFragment()
+    private lateinit var console: ConstraintLayout
+    private lateinit var blocksPanel: ConstraintLayout
+    private lateinit var bindingWorkspace: FragmentWorkspaceBinding
+    private lateinit var bindingConsole: FragmentRightPanelBinding
+    private lateinit var bindingBlocksPanel: FragmentBlocksBinding
+    private val stackOfBlocks = mutableListOf<View>()
     
     
     @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        bindingWorkspace = FragmentWorkspaceBinding.bind(view)
+        bindingConsole = FragmentRightPanelBinding.bind(view)
+        bindingBlocksPanel = FragmentBlocksBinding.bind(view)
         
-        createFragment(fragmentPalette, R.id.container)
-        hideFragment(fragmentPalette)
         isConsoleHidden = true
+        isBlocksPanelHidden = true
         
-        val button = view.findViewById<Button>(R.id.blocksButton)
-        button.setOnClickListener {
-            if (fragmentPalette.isHidden && isConsoleHidden) {
-                takeFragmentFromBottom(fragmentPalette)
+        bindingBlocksPanel.closeButton.setOnClickListener { moveBlocksFragment(300) }
+        bindingBlocksPanel.blocksButton.setOnClickListener {
+            if (isBlocksPanelHidden && isConsoleHidden) {
+                moveBlocksFragment(400)
             }
         }
-    
-        console = view.findViewById(R.id.console)
-        val button2 = view.findViewById<Button>(R.id.button_panel)
-        button2.setOnTouchListener(getDragNDrop())
-    
-        val consoleButton = view.findViewById<Button>(R.id.consoleButton)
-        if(isButtonForConsoleVisibility) {
-            consoleButton.setOnClickListener {
-                if(fragmentPalette.isHidden) {
+        
+        blocksPanel = bindingBlocksPanel.blocksContainer
+        blocksPanel.y = metrics.bounds.height().toFloat()
+        console = bindingConsole.console
+        bindingConsole.buttonPanel.setOnTouchListener(getDragNDrop())
+        
+        if (isButtonForConsoleVisibility) {
+            bindingConsole.consoleButton.setOnClickListener {
+                if (isBlocksPanelHidden) {
                     isPanelMoving = true
-                    isConsoleHidden = if (console.x < 0.6 * metrics.bounds.width()) {
-                        moveContainer(console.x, metrics.bounds.width() - button2.width.toFloat(), console)
-                        true
-                    } else {
-                        moveContainer(console.x, 0.2f * metrics.bounds.width(), console)
+                    isConsoleHidden = if (console.x > 0.6 * metrics.bounds.width()) {
+                        takeConsole(bindingConsole.buttonPanel, 200)
                         false
+                    } else {
+                        hideConsole(bindingConsole.buttonPanel, 200)
+                        true
                     }
+                    isPanelMoving = false
                 }
             }
+        } else {
+            bindingConsole.consoleButton.visibility = View.INVISIBLE
         }
-        else {
-            consoleButton.visibility = View.INVISIBLE
+        for (i in 0 until bindingBlocksPanel.listOfBlocks.childCount) {
+//            bindingBlocksPanel.listOfBlocks.getChildAt(i).setOnClickListener { button ->
+//
+//            }
         }
     }
     
-    private fun createFragment(fragment: Fragment, containerId: Int) {
-        val transaction = childFragmentManager.beginTransaction()
-        transaction.add(containerId, fragment).commit()
-    }
-    private fun hideFragment(fragment: Fragment) {
-        val transaction = childFragmentManager.beginTransaction()
-        transaction.hide(fragment).commit()
-    }
+    private fun createBlockByClickedButton(button: Button): View =
+        when (button) {
+            bindingBlocksPanel.closeButton -> bindingBlocksPanel.panel
+            else -> bindingBlocksPanel.panel
+        }
     
-    private fun takeFragmentFromBottom(fragment: Fragment) {
-        val transaction = childFragmentManager.beginTransaction()
-        transaction.setCustomAnimations(R.anim.slide_in_bottom, 0, 0, R.anim.slide_out_bottom)
-        fragment.onCreateAnimation(0, true, 1)
-        transaction.addToBackStack(null)
-        transaction.show(fragment).commit()
+    private fun addBlockToStack(view: View) {
+        stackOfBlocks.add(view)
     }
     
     @SuppressLint("ClickableViewAccessibility", "SetTextI18n")
     private fun getDragNDrop() = View.OnTouchListener { view, event ->
-        if(fragmentPalette.isHidden) {
+        if (isBlocksPanelHidden) {
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     if (!isPanelMoving) {
@@ -101,32 +111,63 @@ class WorkspaceFragment : Fragment(R.layout.fragment_workspace) {
                     if (!isPanelMoving) {
                         isPanelMoving = true
                         isConsoleHidden = if (console.x > 0.6 * metrics.bounds.width()) {
-                            moveContainer(console.x, metrics.bounds.width() - view.width.toFloat(), console)
+                            hideConsole(view, 200)
                             true
                         } else {
-                            moveContainer(console.x, 0.2f * metrics.bounds.width(), console)
+                            takeConsole(view, 200)
                             false
                         }
+                        isPanelMoving = false
                     }
                     true
                 }
             }
-        }
-        else true
+        } else true
     }
     
-    private fun moveContainer(from:Float, to:Float, container:FrameLayout) {
-        var count = 0
-        object : CountDownTimer(250, 1) {
+    private fun moveBlocksFragment(time: Long) {
+        isBlocksPanelHidden = if (isBlocksPanelHidden) {
+            val from = Point(blocksPanel.x, blocksPanel.y)
+            val to = Point(blocksPanel.x, 0.05f * metrics.bounds.height())
+            moveContainer(from, to, time, blocksPanel)
+            false
+        } else {
+            val from = Point(blocksPanel.x, blocksPanel.y)
+            val to = Point(blocksPanel.x, metrics.bounds.height().toFloat())
+            moveContainer(from, to, time, blocksPanel)
+            true
+        }
+    }
+    
+    private fun takeConsole(view: View, time: Long) {
+        val from = Point(console.x, console.y)
+        val to = Point(0.2f * metrics.bounds.width(), console.y)
+        moveContainer(from, to, time, console)
+    }
+    
+    private fun hideConsole(view: View, time: Long) {
+        val from = Point(console.x, console.y)
+        val to = Point(metrics.bounds.width() - view.width.toFloat(), console.y)
+        moveContainer(from, to, time, console)
+    }
+    
+    private fun moveContainer(from: Point, to: Point, time: Long, container: ConstraintLayout) {
+        object : CountDownTimer(time, 1) {
             override fun onTick(millisUntilFinished: Long) {
-                count++
-                if(count<15) {
-                    container.x += (to - from) / 15
+                val newPoint = Point(container.x + (to.x - from.x) / (time * 60f / 1000f),
+                                     container.y + (to.y - from.y) / (time * 60f / 1000f))
+                val direction = Point(to.x - from.x, to.y - from.y)
+                if( newPoint.x * direction.x <= to.x * direction.x) {
+                    container.x += (to.x - from.x) / (time * 60f / 1000f)
+                }
+                if( newPoint.y * direction.y <= to.y * direction.y) {
+                    container.y += (to.y - from.y) / (time * 60f / 1000f)
                 }
             }
+            
             override fun onFinish() {
-                container.x = to
-                isPanelMoving = false
+                container.x = to.x
+                container.y = to.y
             }
         }.start()
     }
