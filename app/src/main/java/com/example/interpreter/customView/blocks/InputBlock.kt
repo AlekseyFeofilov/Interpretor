20f3c440231cf74b538cb170d7ac1d2e939820b8
Aleksey Feofilov <Alexfil888@gmail.com>
2022-05-21 03:02:28 +0700

    add InputBlock

--- app/src/main/java/com/example/interpreter/customView/blocks/InputBlock.kt
+++ app/src/main/java/com/example/interpreter/customView/blocks/InputBlock.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.util.AttributeSet
 import com.example.interpreter.customView.blockView.BlockView
 import com.example.interpreter.ioInterfaces.IO
-import com.example.interpreter.ioInterfaces.ioTypes.InputString
+import com.example.interpreter.ioInterfaces.ioTypes.InputBoolean
+import com.example.interpreter.ioInterfaces.ioTypes.OutputAny
+import com.example.interpreter.ioInterfaces.ioTypes.OutputBoolean
 import com.example.interpreter.vm.Compiler
-import com.example.interpreter.vm.instruction.*
-import com.example.interpreter.vm.instruction.Number
+import com.example.interpreter.vm.instruction.Input
+import com.example.interpreter.vm.instruction.Instruction
 
 class InputBlock @JvmOverloads constructor(
     context: Context,
@@ -16,64 +18,12 @@ class InputBlock @JvmOverloads constructor(
 ) : BlockView(context, attrs, defStyleAttr) {
     override fun compile(compiler: Compiler): List<Instruction> {
         super.compile(compiler)
-        return requestVariables(compiler)
-    }
-    
-    private fun toBool(string: String): Boolean {
-        return when {
-            string.matches("""\s*true\s*""".toRegex()) -> true
-            string.matches("""\s*false\s*""".toRegex()) -> false
-            else -> {
-                val value = """\s*(\S*)""".toRegex().find(string)
-                throw Error("${value!!.groups[1]!!.value} isn't boolean value")
-            }
-        }
-    }
-    
-    @Suppress("UNREACHABLE_CODE")
-    private fun requestVariables(compiler: Compiler): MutableList<Instruction> {
-        val initializationList = mutableListOf<Instruction>()
-        
-        inputs.forEach { it ->
-            if (it.first !is InputString) return@forEach
-            val value = (it.first as InputString).default
-            
-            if (value != null) {
-                val initializations = value.split(",")
-                
-                initializations.forEach { initialization ->
-                    val variable = """^\s*(\S+)\s*$""".toRegex().find(initialization)
-                    
-                    if (initialization != variable?.groups?.get(0)?.value) throw Error("incorrect expression $initialization")
-                    
-                    TODO("make request to console here")
-                    val instruction = when (it.first.name) {
-                        IO.Name.Double, IO.Name.Int -> Math(compiler, TODO("value") as String)
-                        IO.Name.String -> String(compiler, TODO("value") as String)
-                        else -> Bool(compiler, toBool(TODO("value") as String))
-                    }
-                    
-                    initializationList.add(
-                        SetVar(
-                            compiler,
-                            variable.groups[1]!!.value,
-                            instruction,
-                            true
-                        )
-                    )
-                }
-            }
-        }
-        
-        return initializationList
+        return listOf(Input(compiler))
     }
     
     init {
-        addInput(InputString(IO.Name.Int, this, "Int:", true, isLink = false))
-        addInput(InputString(IO.Name.Double, this, "Double:", true, isLink = false))
-        addInput(InputString(IO.Name.String, this, "String:", true, isLink = false))
-        addInput(InputString(IO.Name.Boolean, this, "Boolean:", true, isLink = false))
+        addOutput(OutputAny(IO.Name.out, this, "value:"))
         
-        setHeader("Input", "#8F95D3")
+        setHeader("Input", "#6CD4FF")
     }
 }
---
+++ app/src/main/java/com/example/interpreter/customView/blocks/InputVarBlock.kt
@@ -0,0 +1,79 @@
+package com.example.interpreter.customView.blocks
+
+import android.content.Context
+import android.util.AttributeSet
+import com.example.interpreter.customView.blockView.BlockView
+import com.example.interpreter.ioInterfaces.IO
+import com.example.interpreter.ioInterfaces.ioTypes.InputString
+import com.example.interpreter.vm.Compiler
+import com.example.interpreter.vm.instruction.*
+import com.example.interpreter.vm.instruction.Number
+
+class InputVarBlock @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : BlockView(context, attrs, defStyleAttr) {
+    override fun compile(compiler: Compiler): List<Instruction> {
+        super.compile(compiler)
+        return requestVariables(compiler)
+    }
+    
+    private fun toBool(string: String): Boolean {
+        return when {
+            string.matches("""\s*true\s*""".toRegex()) -> true
+            string.matches("""\s*false\s*""".toRegex()) -> false
+            else -> {
+                val value = """\s*(\S*)""".toRegex().find(string)
+                throw Error("${value!!.groups[1]!!.value} isn't boolean value")
+            }
+        }
+    }
+    
+    @Suppress("UNREACHABLE_CODE")
+    private fun requestVariables(compiler: Compiler): MutableList<Instruction> {
+        val initializationList = mutableListOf<Instruction>()
+        
+        inputs.forEach { it ->
+            if (it.first !is InputString) return@forEach
+            val value = (it.first as InputString).default
+            
+            if (value != null) {
+                val initializations = value.split(",")
+                
+                initializations.forEach { initialization ->
+                    val variable = """^\s*(\S+)\s*$""".toRegex().find(initialization)
+                    
+                    if (initialization != variable?.groups?.get(0)?.value) throw Error("incorrect expression $initialization")
+                    
+                    val input =
+                    val instruction = when (it.first.name) {
+                        IO.Name.Double, IO.Name.Int -> Math(compiler, TODO("value") as String)
+                        IO.Name.String -> String(compiler, TODO("value") as String)
+                        else -> Bool(compiler, toBool(TODO("value") as String))
+                    }
+                    
+                    initializationList.add(
+                        SetVar(
+                            compiler,
+                            variable.groups[1]!!.value,
+                            instruction,
+                            true
+                        )
+                    )
+                }
+            }
+        }
+        
+        return initializationList
+    }
+    
+    init {
+        addInput(InputString(IO.Name.Int, this, "Int:", true, isLink = false))
+        addInput(InputString(IO.Name.Double, this, "Double:", true, isLink = false))
+        addInput(InputString(IO.Name.String, this, "String:", true, isLink = false))
+        addInput(InputString(IO.Name.Boolean, this, "Boolean:", true, isLink = false))
+        
+        setHeader("Input variable", "#8F95D3")
+    }
+}
