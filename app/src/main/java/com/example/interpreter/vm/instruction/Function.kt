5a5d7a186dcdb04f8e0e0dab922ab7b01a8de80e
foul11 <admin@arturka.net>
2022-05-20 19:03:23 +0700

    Function instruction

--- app/src/main/java/com/example/interpreter/vm/instruction/Function.kt
+++ app/src/main/java/com/example/interpreter/vm/instruction/Function.kt
@@ -1,5 +1,13 @@
 package com.example.interpreter.vm.instruction
 
-class Function {
-    //TODO: implement
+import com.example.interpreter.vm.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+class Function : Instruction  {
+    override fun exec(env: Env) = sequence<Instruction> {
+        yieldAllLR(blocks?.getOrNull(0)?.exec() ?: throw Error("Runtime error 'if[0]' not condition"))
+    }.iterator()
+    
+    constructor(compiler: Compiler, block: Executor) : super(compiler, listOf(block))
 }
