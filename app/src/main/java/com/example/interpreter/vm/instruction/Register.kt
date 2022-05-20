b9a939a8ca7fd827f67fd228fca15d3a91dcae14
foul11 <admin@arturka.net>
2022-05-21 04:06:34 +0700

    feather register

--- app/src/main/java/com/example/interpreter/vm/instruction/Register.kt
+++ app/src/main/java/com/example/interpreter/vm/instruction/Register.kt
@@ -2,6 +2,7 @@ package com.example.interpreter.vm.instruction
 
 import com.example.interpreter.vm.Compiler
 import com.example.interpreter.vm.Env
+import com.example.interpreter.vm.awaitLR
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -9,8 +10,10 @@ open class Register : Instruction {
     private val value: Instruction
     private val objItem: kotlin.String
     private val env: Env?
+    private val _exec: kotlin.Boolean
     
     override fun exec(env: Env) = sequence<Instruction> {
+        if(_exec) awaitLR(value.exec(env))
         val reg = env.reg(value) ?: throw Error("Execution failed, register [${value.types}@${value.id}] is null")
         
         if(reg !is Object){
@@ -24,9 +27,10 @@ open class Register : Instruction {
     
     fun exec(): Iterator<Instruction> { return exec(env ?: throw Error("Env is null")) }
     
-    constructor(compiler: Compiler, value: Instruction, objItem: kotlin.String = "out", env: Env? = null) : super(compiler) {
+    constructor(compiler: Compiler, value: Instruction, objItem: kotlin.String = "out", env: Env? = null, exec: kotlin.Boolean = false) : super(compiler) {
         this.value = value
         this.objItem = objItem
         this.env = env
+        this._exec = exec
     }
 }
