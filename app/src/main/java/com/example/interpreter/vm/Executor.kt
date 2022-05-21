f906148310ed297d950752ae97f2f6cc9456d79f
foul11 <admin@arturka.net>
2022-05-21 10:48:11 +0700

    fix executor

--- app/src/main/java/com/example/interpreter/vm/Executor.kt
+++ app/src/main/java/com/example/interpreter/vm/Executor.kt
@@ -5,7 +5,10 @@ import kotlinx.serialization.Serializable
 
 @Suppress("MemberVisibilityCanBePrivate")
 @Serializable
-class Executor(val env: Env, val tree: MutableList<Instruction>, val localEnv: Boolean = false) {
+class Executor {
+    val env: Env
+    val tree: MutableList<Instruction>
+    val localEnv: Boolean
     
     fun exec() = sequence {
         for(i in tree) {
@@ -14,4 +17,10 @@ class Executor(val env: Env, val tree: MutableList<Instruction>, val localEnv: B
         
         if(localEnv) env.clear()
     }.iterator()
+    
+    constructor(env: Env, tree: List<Instruction>, localEnv: Boolean = false){
+        this.env = env
+        this.tree = tree.toMutableList()
+        this.localEnv = localEnv
+    }
 }
