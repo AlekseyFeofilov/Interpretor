7f872646f43d52646111a7699a8178102bed779c
Aleksey Feofilov <Alexfil888@gmail.com>
2022-05-21 12:19:38 +0700

    get and set object

--- app/src/main/java/com/example/interpreter/customView/blocks/GetObjectBlock.kt
+++ app/src/main/java/com/example/interpreter/customView/blocks/GetObjectBlock.kt
@@ -2,6 +2,7 @@ package com.example.interpreter.customView.blocks
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import com.example.interpreter.customView.blockView.BlockView
 import com.example.interpreter.ioInterfaces.IO
 import com.example.interpreter.ioInterfaces.ioTypes.*
@@ -19,12 +20,42 @@ class GetObjectBlock @JvmOverloads constructor(
 ) : BlockView(context, attrs, defStyleAttr) {
     override fun compile(compiler: Compiler): List<Instruction> {
         super.compile(compiler)
-        return listOf(
-            GetObject(
-                compiler,
-                compiler[IO.Name.Variable],
-                compiler[IO.Name.Key] as Register
-            )
+        return listOf(getObject(compiler))
+    }
+    
+    private fun getObject(compiler: Compiler): GetObject {
+        val inputVariable = getInput((IO.Name.Variable))!!
+        val inputKey = getInput((IO.Name.Key))!!
+        val nameVariable = getLinkInput(inputVariable).name
+        
+        val obj =
+            if (nameVariable == IO.Name.Fake) {
+                compiler.checkVar(
+                    inputVariable.getValue() as kotlin.String?
+                        ?: {
+                            Log.i("TAG", "Missing variable to get object")
+                            throw Error("Missing variable to get object")
+                        }.toString()
+                ) ?: throw Error("Variable ${inputVariable.getValue()} isn't declare")
+            } else {
+                compiler[IO.Name.Variable]
+            }
+    
+        GetVar(
+                    compiler, inputVariable.getValue() as kotlin.String
+                )
+        
+        val key = if (getLinkInput(inputKey).name == IO.Name.Fake) String(
+            compiler,
+            inputKey.getValue() as kotlin.String? ?: throw Error("Missing key to get object")
+        ) else {
+            compiler[IO.Name.Value] as Register
+        }
+        
+        return GetObject(
+            compiler,
+            obj,
+            key
         )
     }
     
